4bc8a8f34b3bc38ad5d966cb3933d71d2f0b4632
Carlos Ponce <carlosponce156@gmail.com>
2020-09-09 13:42:32 -0500

    Create controller to add new car

--- estacionamiento/Model-View/addCarController.swift
+++ estacionamiento/Model-View/addCarController.swift
@@ -9,22 +9,70 @@
 import UIKit
 
 class addCarController: UIViewController {
-
+    
+    @IBOutlet weak var plate: UITextField!
+    @IBOutlet weak var type: UISegmentedControl!
+    @IBOutlet weak var add: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        plate.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addCar(_ segmented: UISegmentedControl) {
+        guard let plateText = plate.text else {
+            showAlert("Ocurrio un problema", "Favor de intentar mas tarde", "ACEPTAR")
+            return
+        }
+        if plateText.count >= 5  {
+            let car = car
+            Cars().appendToCars(<#T##car: Car##Car#>)
+            
+        }else {
+            showAlert("Datos inválidos", "La cantidad de caracteres tienen que ser mayor a 5", "ACEPTAR")
+        }
     }
-    */
+    
+    func getTypeSelected() -> Car.carType {
+        switch type.selectedSegmentIndex {
+        case 0:
+            return .resident
+        case 1:
+            return .noresident
+        default:
+            return .oficial
+        }
+    }
+    
+    func showAlert(_ title: String,_ message: String,_ button: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: button, style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
 
+extension addCarController: UITextFieldDelegate {
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet(charactersIn:"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ").inverted
+        let components = string.components(separatedBy: allowedCharacters)
+        let filtered = components.joined(separator: "")
+
+        let newText = NSString(string: textField.text!).replacingCharacters(in: range, with: string)
+        
+        return string == filtered ? newText.count <= 8 ? true: false : false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        textField.resignFirstResponder()
+    }
 }
